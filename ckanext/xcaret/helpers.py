7e63391e7cfff233020d09606232795ce4ed7ada
Jovanka Gulicoska <jovanka.gulicoska@keitaro.com>
2022-09-02 14:44:46 +0200

    Branding and UI changes

--- ckanext/xcaret/helpers.py
+++ ckanext/xcaret/helpers.py
@@ -30,6 +30,6 @@ def get_featured_pages(limit=3):
     context = {'model': model, 'user': c.user,
                'auth_user_obj': c.userobj}
     pages = p.toolkit.get_action('ckanext_pages_list')(context, data_dict)
-    print (pages)
+    pages = pages[:limit]
     #featured = [page for page in pages if page['featured']]
     return pages[:limit]
